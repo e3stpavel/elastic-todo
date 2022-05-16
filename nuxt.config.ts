b910eb56cc081d64dd1a77500e7c522b9a86710d
e3stpavel <70956582+e3stpavel@users.noreply.github.com>
2022-05-16 18:04:49 +0300

    Windicss added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  alias: {
+    '~': '/<rootDit>',
+  },
+  buildModules: [
+    'nuxt-windicss',
+  ],
 })
---
+++ windi.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'windicss/helpers'
+
+export default defineConfig({
+  extract: {
+    include: ['**/*.{vue,html,jsx,tsx}'],
+    exclude: ['node_modules', '.git'],
+  },
+  theme: {
+
+  },
+})
